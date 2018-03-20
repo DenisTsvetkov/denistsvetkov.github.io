d634cdb77646ec27bc9586029460383ac447500f
Денис Цветков <denis.tsvetkov59@gmail.com>
2018-03-20 12:49:13 +0500

    Сделал липкое меню

--- eurasia/js/nav.js
+++ eurasia/js/nav.js
@@ -1,12 +1,23 @@
 $(function(){
-      var screenHeight = $('.home').height();
-      $(window).scroll(function(){
-        if($(window).scrollTop() >= screenHeight){
-          $(".navigation").addClass('position-fixed');
-        }
-        else{
-          $(".navigation").removeClass('position-fixed');
-        }
-      });
-       
-   });
+
+      navbar();
+
+      function navbar(){
+        var screenHeight = $('.hero').height()+60;
+        $(window).scroll(function(){
+          if($(window).scrollTop() >= screenHeight){
+            $('.show-nav').addClass('show');
+            $(".navigation").addClass('position-fixed');
+          }
+          else{
+            $('.show-nav').removeClass('show');
+            $(".navigation").removeClass('position-fixed');
+          }
+        });
+
+        $('.show-nav-btn').click(function(e){
+          e.preventDefault();
+          $('.position-fixed').toggleClass('show');
+        });
+      };
+});
