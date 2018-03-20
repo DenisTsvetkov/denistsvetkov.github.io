6c9843234012137d71caa25bb0249724c79657d4
Денис Цветков <denis.tsvetkov59@gmail.com>
2018-03-20 15:11:00 +0500

    Исправил блок сотрудничества

--- eurasia/js/nav.js
+++ eurasia/js/nav.js
@@ -1,23 +1,24 @@
 $(function(){
+  var screenHeight = $('.hero').height()+60;
+  navbar();
 
-      navbar();
+  function navbar(){
+    if($(window).scrollTop() >= screenHeight){
+      $('.show-nav').addClass('show');
+      $(".navigation").addClass('position-fixed');
+    }
+    else{
+      $('.show-nav').removeClass('show');
+      $(".navigation").removeClass('position-fixed');
+    }
+        
+    $('.show-nav-btn').click(function(e){
+      e.preventDefault();
+      $('.position-fixed').toggleClass('show');
+    });
+  };
 
-      function navbar(){
-        var screenHeight = $('.hero').height()+60;
-        $(window).scroll(function(){
-          if($(window).scrollTop() >= screenHeight){
-            $('.show-nav').addClass('show');
-            $(".navigation").addClass('position-fixed');
-          }
-          else{
-            $('.show-nav').removeClass('show');
-            $(".navigation").removeClass('position-fixed');
-          }
-        });
-
-        $('.show-nav-btn').click(function(e){
-          e.preventDefault();
-          $('.position-fixed').toggleClass('show');
-        });
-      };
+  $(window).scroll(function(){
+    navbar();
+  });
 });
